3034c74297499c5ece68e05cccfe510cf63bb4d5
John Smith <johnsmith@Johns-Mac.local>
2019-03-20 13:22:21 -0700

    adding printing a placeholder as a card label

--- Set/Set/SetCardView.swift
+++ Set/Set/SetCardView.swift
@@ -10,7 +10,34 @@ import UIKit
 
 class SetCardView: UIView {
 
-    var faceUp = true {didSet: draw}
+    private var faceUp = true { didSet {setNeedsDisplay(); setNeedsLayout()} }
+    
+    func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return NSAttributedString(string: string, attributes: [.font: font, .paragraphStyle: paragraphStyle])
+    }
+    
+    private lazy var textLabel = createCardLabel()
+    
+    private func createCardLabel() -> UILabel {
+        let label = UILabel()
+        label.numberOfLines = 0
+        addSubview(label)
+        return label
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel.attributedText = centeredAttributedString("H\nH\nH", fontSize: 12)
+        textLabel.frame.size = CGSize.zero
+        textLabel.sizeToFit()
+        
+        textLabel.frame.origin = CGPoint(x: bounds.midX, y: bounds.midY)
+    }
     
     override func draw(_ rect: CGRect) {
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 15)
