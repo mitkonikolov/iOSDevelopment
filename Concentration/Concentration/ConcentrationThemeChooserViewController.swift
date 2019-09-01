779e34c2c62d1b56bf06d026357b9db35146cdf8
mitkonikolov <nikolov.m@husky.neu.edu>
2019-09-01 11:21:35 -0400

    feat: add conditional segueing to allow changing themes dynamically both on iPad and iPhone

--- Concentration/Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/Concentration/ConcentrationThemeChooserViewController.swift
@@ -19,8 +19,27 @@ class ConcentrationThemeChooserViewController: UIViewController {
     "Vehicles": ["🚗", "🚌", "🚎", "🏎", "🚓", "✈️", "🛳", "🚁", "🚀", "🚅", "🚃","⛵️"]]
   
   @IBAction func changeTheme(_ sender: Any) {
-    performSegue(withIdentifier: "Choose Theme", sender: sender)
+    if let cvc = splitViewDetailConcentrationViewController {
+      if let themeName = (sender as? UIButton)?.currentTitle, let theme = emojiThemes[themeName] {
+        cvc.emojiChoices = theme
+      }
+    }
+    else if let cvc = lastSeguedToConcentrationViewController {
+      if let themeName = (sender as? UIButton)?.currentTitle, let theme = emojiThemes[themeName] {
+        cvc.emojiChoices = theme
+      }
+      navigationController?.pushViewController(cvc, animated: true)
+    }
+    else {
+      performSegue(withIdentifier: "Choose Theme", sender: sender)
+    }
   }
+  
+  private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+    return splitViewController?.viewControllers.last as? ConcentrationViewController
+  }
+  
+  private var lastSeguedToConcentrationViewController: ConcentrationViewController?
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
@@ -28,9 +47,10 @@ class ConcentrationThemeChooserViewController: UIViewController {
       if let themeName = (sender as? UIButton)?.currentTitle, let theme = emojiThemes[themeName] {
         if let cvc = segue.destination as? ConcentrationViewController {
           cvc.emojiChoices = theme
+          lastSeguedToConcentrationViewController = cvc
         }
       }
-    }
+  }
  
 
 }
