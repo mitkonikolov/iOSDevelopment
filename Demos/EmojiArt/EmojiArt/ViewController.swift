c09dc1245297c773d8c9b1c4f0fcc9372089b974
mitkonikolov <nikolov.m@husky.neu.edu>
2019-09-21 15:25:23 -0400

    chore: initializing a new EmojiArt project

---
+++ Demos/EmojiArt/EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Mitko Nikolov on 9/21/19.
+//  Copyright © 2019 Mitko Nikolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
