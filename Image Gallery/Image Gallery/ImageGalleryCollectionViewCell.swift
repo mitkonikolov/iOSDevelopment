f3a72cca218773585bc17f99d8dc197a929f4077
mitkonikolov <nikolov.m@husky.neu.edu>
2019-09-27 15:42:31 -0400

    feat: add splitview and a collection view and create a custom collection view cell

---
+++ Image Gallery/Image Gallery/ImageGalleryCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageGalleryCollectionViewCell.swift
+//  Image Gallery
+//
+//  Created by Mitko Nikolov on 9/27/19.
+//  Copyright © 2019 Mitko Nikolov. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryCollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet weak var imageView: UIImageView!
+  
+  
+}
---
+++ Image Gallery/Image Gallery/ImageGalleryCollectionViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ImageGalleryCollectionViewController.swift
+//  Image Gallery
+//
+//  Created by Mitko Nikolov on 9/27/19.
+//  Copyright © 2019 Mitko Nikolov. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryViewController: UIViewController, UICollectionViewDataSource {
+  
+  let picture
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageGalleryCell", for: indexPath)
+//    if let imageGalleryCell = cell as? ImageGalleryCollectionViewCell {
+//      if let data
+//    }
+    return cell
+  }
+  
+  
+  @IBOutlet weak var ImageGalleryCollectionView: UICollectionView! {
+    didSet {
+      ImageGalleryCollectionView.dataSource = self
+//      ImageGalleryCollectionView.delegate = self
+    }
+  }
+  
+  
+  
+
+}
